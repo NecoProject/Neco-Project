b40435d0bbdf10a6c22fea39a2d624b70b909ddf
Sébastien Tromp <sebastien.tromp@gmail.com>
2014-08-24 18:19:09 +0200

    Fixed bug on skill prefab

--- Unity/Assets/Scripts/End Screen/GeneticAlgorithm.cs
+++ Unity/Assets/Scripts/End Screen/GeneticAlgorithm.cs
@@ -11,10 +11,10 @@ public class GeneticAlgorithm
 
 		private const int MAX_CHILDREN_NUMBER = 3;
 
-		public List<SkillStats> Evolve (SkillStats father, SkillStats mother, int difficultLevel)
+		public List<SkillStats> Evolve(SkillStats father, SkillStats mother, int difficultLevel)
 		{
-				List<SkillStats> children = Breed (father, mother);
-				Mutate (children, difficultLevel);
+				List<SkillStats> children = Breed(father, mother);
+				Mutate(children, difficultLevel);
 				return children;
 		}
 
@@ -23,43 +23,45 @@ public class GeneticAlgorithm
 		/// I like the idea of each skill having a "genome", i.e. an array of values that then drives its characteristics.
 		/// In other words, have the characteristics (each property) of a skill be computed from the skill "DNA", instead of us assigning values to them
 		/// See IdeasToDiscuss.txt
-		private List<SkillStats> Breed (SkillStats father, SkillStats mother)
+		private List<SkillStats> Breed(SkillStats father, SkillStats mother)
 		{
-				List<SkillStats> children = new List<SkillStats> ();
-				for (int i = 0; i < MAX_CHILDREN_NUMBER; i++) {
-						SkillStats stat = BreedSingleChild (father, mother);
+				List<SkillStats> children = new List<SkillStats>();
+				for (int i = 0; i < MAX_CHILDREN_NUMBER; i++)
+				{
+						SkillStats stat = BreedSingleChild(father, mother);
 						//Debug.Log("Bred: " + stat);
-						children.Add (stat);
+						children.Add(stat);
 				}
 				return children;
 		}
 
-		private SkillStats BreedSingleChild (SkillStats father, SkillStats mother)
+		private SkillStats BreedSingleChild(SkillStats father, SkillStats mother)
 		{
-				SkillStats child = new SkillStats ();
+				SkillStats child = new SkillStats();
 
-				child.Damage = ComputeValue (father.Damage, mother.Damage);
-				child.Cost = ComputeValue (father.Cost, mother.Cost);
+				child.Damage = ComputeValue(father.Damage, mother.Damage);
+				child.Cost = ComputeValue(father.Cost, mother.Cost);
 
 				return child;
 		}
 
-		private float ComputeValue (float fatherValue, float motherValue)
+		private float ComputeValue(float fatherValue, float motherValue)
 		{
-				return UnityEngine.Random.Range (Mathf.Min (fatherValue, motherValue), Mathf.Max (fatherValue, motherValue));
+				return UnityEngine.Random.Range(Mathf.Min(fatherValue, motherValue), Mathf.Max(fatherValue, motherValue));
 		}
 
 		/// Again, very basic implementation
-		private void Mutate (List<SkillStats> children, int difficultyLevel)
+		private void Mutate(List<SkillStats> children, int difficultyLevel)
 		{
-				foreach (SkillStats child in children) {
-						Mutate (child, difficultyLevel);
+				foreach (SkillStats child in children)
+				{
+						Mutate(child, difficultyLevel);
 				}
 		}
 
 		/// Mutate the provided child's statistics taking into account the difficulty. The higher the difficulty, the higher the mutation effects. 
 		/// TODO: Do we want to center the mutation around 0? Statistically we may result in pure better or pure worse stats afterwards.
-		private void Mutate (SkillStats child, int difficultLevel)
+		private void Mutate(SkillStats child, int difficultLevel)
 		{
 				child.Damage = child.Damage + child.Damage * UnityEngine.Random.Range(-BASE_BONUS_MAX, BASE_BONUS_MAX) * difficultLevel;
 				child.Cost = child.Cost + child.Cost * UnityEngine.Random.Range(-BASE_BONUS_MAX, BASE_BONUS_MAX) * difficultLevel;
---
+++ Unity/Assets/Scripts/HUD/Health.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Health : MonoBehaviour
+{
+		private PlayerScript _player;
+		private TextMesh _display;
+
+		void Start()
+		{
+				_player = FindObjectOfType<PlayerScript>();
+				_display = GetComponent<TextMesh>();
+		}
+
+		// TODO: maybe better to not redraw every frame, but rather only when the health changes
+		void Update()
+		{
+				_display.text = _player.Stats.CurrentHealth + " / " + _player.Stats.MaxHealth;
+		}
+}
