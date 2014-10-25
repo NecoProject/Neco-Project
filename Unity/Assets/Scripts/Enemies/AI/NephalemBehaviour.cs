b6aa94f2cf94ce8fd715d9451beb04883d9c5048
Sébastien Tromp <sebastien.tromp@gmail.com>
2014-10-25 23:53:27 +0200

    And can take again the skill from the boss

--- Unity/Assets/Scripts/Enemies/AI/NephalemBehaviour.cs
+++ Unity/Assets/Scripts/Enemies/AI/NephalemBehaviour.cs
@@ -3,32 +3,6 @@ using System.Collections;
 
 public class NephalemBehaviour : EnemyBehaviour
 {
-		// The Nephalem has two different attacks: a standard attack, and 
-		// an attack that drains life
-		/*public StandardAttack StandardAttack;
-		public NephalemNova NephalemNova;
-
-		private EnemySkill _currentSkill;
-
-		protected override void PerformAttack()
-		{
-				if (NephalemNova.CanUse())
-				{
-						_currentSkill = NephalemNova;
-				}
-				else
-				{
-						_currentSkill = StandardAttack;
-				}
-
-				_currentSkill.Attack(_target, _damage);
-		}
-
-		protected override IEnumerator AnimateAttack()
-		{
-				return _currentSkill.Animate(_target);
-		}*/
-
 		// Don't really like this, should refactor with better design. Need brainstorm!
 		void OnDestroy()
 		{
